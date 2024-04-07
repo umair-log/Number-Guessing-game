0d45cd0323a60ef1ecc856c7896363d40ff45e09
Umair Butt <buttjeefull77@gmial.com>
2024-04-07 13:04:47 +0500

    bit update

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
-//  1) Computer will generate a random number.
-//  2) User input for guessing number.
-//  3) Compare user input with computer generated number and show result.
 let random = Math.floor(Math.random() * 6 + 1);
-console.log(random);
-const answer = await inquirer.prompt([
-    { message: "Guess a number from 1 to 6", type: "number", name: "number" },
-]);
-console.log(answer.number);
-if (answer.number > 0 && answer.number < 7) {
-    if (answer.number === random) {
-        console.log("Congrates you win");
+// console.log(random);
+async function start() {
+    console.log(' \n');
+    const menu = await inquirer.prompt([
+        { message: "Menu", type: "list", name: "option", choices: [
+                "Start Game", "End Game"
+            ] },
+    ]);
+    if (menu.option === "Start Game") {
+        // game logic
+        console.log(' \n');
+        const answer = await inquirer.prompt([
+            { message: "Guess a number from 1 to 6", type: "number", name: "number" },
+        ]);
+        if (answer.number > 0 && answer.number < 7) {
+            if (answer.number === random) {
+                console.log("Congrates you win");
+            }
+            else {
+                console.log('Better luck next time');
+            }
+        }
+        else {
+            console.log("Please try to guess a valid number");
+        }
+        start();
+        // game end block
     }
     else {
-        console.log('Better luck next time');
+        console.log('Bye .....');
     }
 }
-else {
-    console.log("Please try to guess a valid number");
-}
+start();
--- index.ts
+++ index.ts
@@ -1,28 +1,49 @@
 #!/usr/bin/env node
 import inquirer from "inquirer";
 
-//  1) Computer will generate a random number.
-//  2) User input for guessing number.
-//  3) Compare user input with computer generated number and show result.
-
 let random = Math.floor(Math.random() * 6 + 1);
 
-console.log(random);
+// console.log(random);
 
-const answer = await inquirer.prompt([
-  { message: "Guess a number from 1 to 6", type: "number", name: "number" },
-]);
+async function start(){
 
-// console.log(answer.number);
+  console.log(' \n');
+  const menu = await inquirer.prompt([
+    { message: "Menu", type: "list", name: "option", choices: [
+      "Start Game", "End Game"
+    ]  },
+  ]);
 
-if (answer.number > 0 && answer.number < 7) {
-  if (answer.number === random) {
-    console.log("Congrates you win");
+  if(menu.option === "Start Game"){
+    // game logic
+    console.log(' \n');
+    
+    const answer = await inquirer.prompt([
+      { message: "Guess a number from 1 to 6", type: "number", name: "number" },
+    ]);
+    
+    if (answer.number > 0 && answer.number < 7) {
+      if (answer.number === random) {
+        console.log("Congrates you win");
+      }
+      else{
+        console.log('Better luck next time');
+      }
+    
+    } else {
+      console.log("Please try to guess a valid number");
+    }
+    start();
+
+  // game end block
   }
   else{
-    console.log('Better luck next time');
+    console.log('Bye .....');
   }
-
-} else {
-  console.log("Please try to guess a valid number");
+  
 }
+
+start();
+
+
+
