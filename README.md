83dbd187b851d8ce3d63396167806b49fe3afa14
Umair Butt <buttjeefull77@gmial.com>
2024-04-07 12:46:24 +0500

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+To use run this command : "npx umair-number-guessing"
+
+Simple number guessing CLI game build by using typescript
---
+++ index.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+
+//  1) Computer will generate a random number.
+//  2) User input for guessing number.
+//  3) Compare user input with computer generated number and show result.
+
+let random = Math.floor(Math.random() * 6 + 1);
+
+console.log(random);
+
+const answer = await inquirer.prompt([
+  { message: "Guess a number from 1 to 6", type: "number", name: "number" },
+]);
+
+// console.log(answer.number);
+
+if (answer.number > 0 && answer.number < 7) {
+  if (answer.number === random) {
+    console.log("Congrates you win");
+  }
+  else{
+    console.log('Better luck next time');
+  }
+
+} else {
+  console.log("Please try to guess a valid number");
+}
